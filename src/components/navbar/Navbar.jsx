a72bc03e7a5fe722c3a53bf56a9fb64518bc49e9
juanpablosys <juanpablosys@gmail.com>
2022-07-19 23:59:05 -0300

    Incorporando menu de e-comerce

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+  )
+}
+
+export default navbar
