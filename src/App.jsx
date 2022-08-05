305e70325a1195552007584dbf1addcc7703b63b
juanpablosys <juanpablosys@gmail.com>
2022-08-05 00:13:34 -0300

    Priemra entrega del proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
    return (
       <>
-         <NavBar />
-         <ItemListContainer />
+         <BrowserRouter>
+            <NavBar />
+            <Routes>
+               <Route path="/" element={<ItemListContainer />} />
+               <Route path="/test" element={<p>Este es un test</p>} />
+               <Route path="/algo" element={<p>Aca va algo</p>} />
+               <Route path="*" element={<p>404</p>} />
+            </Routes>
+         </BrowserRouter>
       </>
    );
 }
